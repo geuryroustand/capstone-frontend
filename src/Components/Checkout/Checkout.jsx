559cd54cbc8eaa72180e20955c689235b5b62dfd
Geury <theromantic2010@hotmail.com>
2021-11-15 10:32:36 +0100

    Update Checkout.jsx

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -62,6 +62,9 @@ export const Checkout = () => {
             name: `Private Airport Transfers`,
             description: `Transfer from ${taxiSelected.pickUpLocation} To ${taxiSelected.dropLocation}`,
           },
+          metadata: {
+            test: "Hu",
+          },
         },
       },
     ];
@@ -69,9 +72,7 @@ export const Checkout = () => {
     const response = await handlerPayment({
       line_items,
       customer_email: taxiSelected.email,
-      metadata: {
-        test: "Hu",
-      },
+
       // name,
       // surname,
       // phoneNumber,
